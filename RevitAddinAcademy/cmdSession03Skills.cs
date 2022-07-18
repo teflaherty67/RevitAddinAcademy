d1479f44a4051affd5e299cfab13aca81d362440
Tom E Flaherty <teflaherty@live.com>
2022-07-18 16:35:53 -0500

    Session03Skills started

---
+++ RevitAddinAcademy/cmdSession03Skills.cs
@@ -0,0 +1,37 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Excel = Microsoft.Office.Interop.Excel;
+using Forms = System.Windows.Forms;
+
+#endregion
+
+namespace RevitAddinAcademy
+{
+    [Transaction(TransactionMode.Manual)]
+    public class cmdSession03Skills : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            Forms.OpenFileDialog dialog = new Forms.OpenFileDialog();
+            dialog.InitialDirectory = @"c:\";
+
+            
+            return Result.Succeeded;
+        }
+    }
+}
