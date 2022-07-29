8f798a0fdb1f3c8202299f6b62c6bf51d53736bc
Tom E Flaherty <teflaherty@live.com>
2022-07-29 14:11:13 -0500

    Bonus In Progress

---
+++ RevitAddinAcademy/formWallsFromLines.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RevitAddinAcademy
+{
+    public partial class formWallsFromLines : Form
+    {
+        public formWallsFromLines()
+        {
+            InitializeComponent();
+        }
+    }
+}
